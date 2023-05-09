5a1e53c64b83e053477866739b83944331bfe7d7
RonanKMcGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2023-05-09 13:53:42 +0100

    add .env

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 
 function App() {
   const [count, setCount] = useState(0)
